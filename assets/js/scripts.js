5562024bcabf3dffe53fa8bfb3036e37e876b251
TomRchd25 <155908650+TomRchd25@users.noreply.github.com>
2025-03-07 15:30:45 +0100

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,17 @@
+function zoomDocument(type) {
+    const modal = new bootstrap.Modal(document.getElementById('documentModal'));
+    const imgElement = document.getElementById('fullDocument');
+    
+    if(type === 'cv') {
+        imgElement.src = 'assets/img/cv-full.jpg';
+    } else if(type === 'ldm') {
+        imgElement.src = 'assets/img/ldm-full.jpg';
+    }
+    
+    modal.show();
+}
 
+// Gestionnaire d'événement pour la fermeture du modal
+document.getElementById('documentModal').addEventListener('hidden.bs.modal', function () {
+    document.getElementById('fullDocument').src = '';
+});
